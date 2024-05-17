18c86d2286f9b93ed571c2db6938eb5f5ad98aa0
Elian <ecardonap5@miumg.edu.gt>
2024-05-16 23:23:22 -0600

    Commitng changes to main.py

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 from flask import Flask, render_template, request, redirect
 import psycopg2
+from psycopg2 import sql
 
-conn = psycopg2.connect(
-    host='localhost',
-    user='postgres',
-    password='V16$',
-    database='CLASE_IDB'
-)
+class database:
+    def __init__(self, nombre_db,usuario, contraseña,host='localhost'):
+        self.nombre_db = nombre_db,
+        self.contraseña = contraseña,
+        self.host = host,
+        self.usuario = usuario
+        self.connection = None
 
-cursor = conn.cursor()
+    def connect(self):
+        self.connection = psycopg2.connect(
+            nombre_db = self.nombre_db,
+            contraseña = self.contraseña,
+            usuario = self.usuario,
+            host = self.host
+        )
+    
+    def close(self):
+        if self.connection:
+            self.connection.close()
+
+class InsertRecord(database):
+    def insert(self, table, data):
+        try:
+            self.connect()
+            with self.connection.cursor() as cursor:
+                columns =   data.keys()
+                values = [data[column] for column in columns]
+                insert_query = sql.SQL("INSERT INTO {table} ({fields}) VALUES ({values})").format(
+                    table=sql.Identifier(table),
+                    fields=sql.SQL(',').join(map(sql.Identifier, columns)),
+                    values=sql.SQL(',').join(sql.Placeholder() * len(values))
+                )
+                cursor.execute(insert_query, values)
+                self.connection.commit()
+        except Exception as e:
+            print(f"Error inserting record: {e}")
+        finally:
+            self.close()
 
 app = Flask(__name__)
 
