064209de09b182c856fc2d58a6c779981bcc9dde
Elian <ecardonap5@miumg.edu.gt>
2024-05-18 12:03:08 -0600

    Commiting the form

--- main.py
+++ main.py
@@ -47,5 +47,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/alumnos')
+def alumnos():
+    return render_template("alumnos.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
