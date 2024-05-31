2991e9234231f68e9fdc29d6a4063eeed8a0bef0
Elian <ecardonap5@miumg.edu.gt>
2024-05-31 00:48:15 -0600

    Aplicacion funcional, falta funcion editar

--- main.py
+++ main.py
@@ -13,6 +13,29 @@ class Database:
 
         self.cursor = self.conn.cursor()
 
+    def obtener_registros(self, table):
+        try:
+            #self.cursor.execute(f"SELECT id, nombre, apellido, edad FROM {table}")
+            self.cursor.execute(f"SELECT * FROM {table}")
+            rows = self.cursor.fetchall()
+            columns = [desc[0] for desc in self.cursor.description]
+            return [dict(zip(columns, row)) for row in rows]
+            #return self.cursor.fetchall()
+        except Exception as e:
+            print(f"Error extrayendo registros: {e}")
+            return[]
+
+    def editar_registro(self,table, data, condition):
+        try:
+            set_clause = '. '.join([f"{key} = %s" for key in data.keys()])
+            values = tuple(data.values())
+            update_query = f"UPDATE {table} SET {set_clause} WHERE {condition}"
+            self.cursor.execute(update_query, values)
+            print(f"Executing query: {update_query} with values: {values}")
+            self.conn.commit()
+        except Exception as e:
+            print(f"Error al editar registro: {e}")
+
     def insertar_registro(self, table, data):
         try:
             columns = data.keys()
@@ -31,12 +54,29 @@ class Database:
             self.conn.commit()
         except Exception as e:
             print(f"Error en ingresar el registro: {e}")
+
+    def eliminar_registro(self, table, condition):
+        try:
+            delete_query = sql.SQL(
+                "DELETE FROM {table} WHERE {condition}"
+            ).format(
+                table=sql.Identifier(table),
+                condition=sql.SQL(condition)
+            )
+            print(delete_query.as_string(self.conn))
+            self.cursor.execute(delete_query)
+            self.conn.commit()
+        except Exception as e:
+            print(f"Error al eliminar: {e}")
+        finally:
+            self.close_connection()
     
     def close_connection(self):
             self.cursor.close()
             self.conn.close()
 
 app = Flask(__name__)
+app.debug=True
 
 @app.route('/')
 def index():
@@ -44,11 +84,47 @@ def index():
 
 @app.route('/alumnos')
 def alumnos():
-    return render_template("alumnos.html")
+    bd = Database()
+    alumnos = bd.obtener_registros('alumnos')
+    bd.close_connection()
+    return render_template("alumnos.html", alumnos=alumnos)
 
 @app.route('/profesores')
 def profesores():
-    return render_template("profesores.html")
+    bd = Database()
+    profesores = bd.obtener_registros('profesores')
+    bd.close_connection()
+    return render_template("profesores.html", profesores=profesores)
+
+@app.route('/profesores/eliminar_profesor/<id_profesor>', methods=['POST'])
+def eliminar_profesor(id_profesor):
+    bd = Database()
+    condition = f"id_profesor = '{id_profesor}'"
+    bd.eliminar_registro('profesores', condition)
+    bd.close_connection()
+    return redirect('/profesores')
+
+@app.route('/profesores/editar_profesor/<id_profesor>', methods=['GET', 'POST'])
+def editar_profesor(id_profesor):
+    bd = Database()
+    condition = f"id_profesor = '{id_profesor}'"
+    if request.method == 'POST':
+        nombre_profesor = request.form['nombre_profesor']
+        materia_profesor = request.form['materia_profesor']
+
+        data = {
+            'nombre_profesor': nombre_profesor,
+            'materia_profesor': materia_profesor
+        }
+
+        bd.editar_registro('profesores', data, condition)
+        bd.close_connection()
+        return redirect('/profesores')
+    
+    profesor = bd.obtener_registros('profesores')
+    profesor = next((p for p in profesor if p['id_profesor'] == id_profesor), None)
+    bd.close_connection()
+    return render_template('editar_profesor.html', profesor=profesor)
 
 @app.route('/cursos')
 def cursos():
@@ -80,6 +156,13 @@ def agregar_alumno():
         return redirect('/alumnos')
     return render_template("agregar_alumno.html")
 
+@app.route('/alumnos/eliminar_alumno/<int:id>', methods=['POST'])
+def eliminar_alumno(id):
+    bd = Database()
+    condition = f"id = {id}"
+    bd.eliminar_registro('alumnos', condition)
+    return redirect('/alumnos')
+
 @app.route('/cursos/agregar_curso', methods=['GET', 'POST'])
 def agregar_curso():
     if request.method == 'POST':
@@ -105,14 +188,12 @@ def agregar_curso():
 @app.route('/profesores/agregar_profesor', methods=['GET', 'POST'])
 def agregar_profesor():
     if request.method == 'POST':
-        id_profesor = request.form['id_profesor']
         nombre_profesor = request.form['nombre_profesor']
         materia_profesor = request.form['materia_profesor']
 
         bd = Database()
     
         data = {
-            'id_profesor': id_profesor,
             'nombre_profesor': nombre_profesor,
             'materia_profesor': materia_profesor
         }
